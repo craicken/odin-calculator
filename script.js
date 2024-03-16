53c78f0dc87217131eae7548c60ab4cc8586f56c
jess <162760617+craicken@users.noreply.github.com>
2024-03-16 23:48:07 +0000

    add menu to evaluate operator and call relevant function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let num1;
+let num2;
+let operator;
+
+
 function add (num1, num2) {
     return num1 + num2;
 };
@@ -12,4 +17,17 @@ function multiply (num1, num2) {
 
 function divide (num1, num2) {
     return num1 / num2;
+};
+
+function operate (num1, operator, num2) {
+    switch(operator) {
+        case "+":
+            add(num1, num2);
+        case "-":
+            subtract(num1, num2);
+        case "*":
+            multiply(num1, num2);
+        case "/":
+            divide(num1, num2);
+    }
 };
